03972cd1d79334dc631f3b522201073bbe20a1fe
User <User@DESKTOP-TEV60K7>
2023-08-11 23:10:40 -0500

    Creación de pantalla menú principal

--- src/main/java/espol/poo/proyectopoogrupo2/fxml/PrimaryController.java
+++ src/main/java/espol/poo/proyectopoogrupo2/fxml/PrimaryController.java
@@ -2,11 +2,26 @@ package espol.poo.proyectopoogrupo2.fxml;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+    @FXML
+    private Button btconf;
+    @FXML
+    private Button btnew;
+    @FXML
+    private Button btreport;
+    @FXML
+    private Button btsalir;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    @FXML
+    private void switchToconf() throws IOException {
+        App.setRoot("conf");
+    }
+
 }
