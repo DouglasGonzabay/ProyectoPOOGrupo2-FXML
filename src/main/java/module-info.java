155cae5bc02561864fc46e1813bfced5e0cae726
User <User@DESKTOP-TEV60K7>
2023-08-13 00:11:59 -0500

    Actualización

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module espol.poo.proyectopoogrupo2.fxml {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens espol.poo.proyectopoogrupo2.fxml to javafx.fxml;
     exports espol.poo.proyectopoogrupo2.fxml;
