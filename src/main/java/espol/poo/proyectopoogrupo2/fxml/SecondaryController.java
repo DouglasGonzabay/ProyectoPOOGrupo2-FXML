342eadede5d4819ce7fb05294a2a24eb89a56f92
User <User@DESKTOP-TEV60K7>
2023-08-15 22:04:26 -0500

    Modificacion del secondaryController

--- src/main/java/espol/poo/proyectopoogrupo2/fxml/SecondaryController.java
+++ src/main/java/espol/poo/proyectopoogrupo2/fxml/SecondaryController.java
@@ -1,10 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package espol.poo.proyectopoogrupo2.fxml;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class SecondaryController {
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import java.io.*;
+/**
+ * FXML Controller class
+ *
+ * @author Daniela Basilio
+ */
+public class SecondaryController implements Initializable {
 
+
+    @FXML
+    private Button btatras;
+    @FXML
+    private Button btempezar;
+    @FXML
+    private ComboBox<?> slmateria;
+    @FXML
+    private ComboBox<?> slparalelo;
+    @FXML
+    private TextField ingnivel;
+    @FXML
+    private VBox vbparticipante;
+    @FXML
+    private VBox vbcompanero;
+    @FXML
+    private Button btazar;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }  
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
