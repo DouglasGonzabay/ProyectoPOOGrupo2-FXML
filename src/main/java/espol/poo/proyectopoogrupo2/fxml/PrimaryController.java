6b88199efa722a5fde30c90705e63d6857473a16
User <User@DESKTOP-TEV60K7>
2023-08-22 21:33:09 -0500

    configuración de juego para bloquear boton de nuevo juego si no se tiene termino configurado

--- src/main/java/espol/poo/proyectopoogrupo2/fxml/PrimaryController.java
+++ src/main/java/espol/poo/proyectopoogrupo2/fxml/PrimaryController.java
@@ -1,12 +1,20 @@
 package espol.poo.proyectopoogrupo2.fxml;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import modelo.NewClass;
+import modelo.TerminoAcademico;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
     @FXML
     private Button btconf;
@@ -16,6 +24,30 @@ public class PrimaryController {
     private Button btreport;
     @FXML
     private Button btsalir;
+    @FXML
+    private BorderPane border;
+    
+    /**
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       TerminoAcademico terJuego = NewClass.terminoConfigurado();
+       VBox v = new VBox(5);
+       if(terJuego.getAnio().equals("")||terJuego.getNumero().equals("")){
+           btnew.setDisable(true);
+           Label l = new Label("NOTA: Configure el término académico antes de jugar");
+           v.getChildren().addAll(l,new Label(terJuego.toString()+" Termino No Configurado"));
+           border.setBottom(v);
+       }else{
+           btnew.setDisable(false);
+           v.getChildren().clear();
+           v.getChildren().addAll(new Label(terJuego.toString()+" Termino Configurado"));
+           border.setBottom(v);
+       }
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
