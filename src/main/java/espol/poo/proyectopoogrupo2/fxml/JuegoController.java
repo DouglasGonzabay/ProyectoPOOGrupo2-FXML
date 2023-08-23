26cac6bbb78540b3784be09e1a5bde4f9112a954
User <User@DESKTOP-TEV60K7>
2023-08-23 15:58:19 -0500

    Controlador de Juego añadido

---
+++ src/main/java/espol/poo/proyectopoogrupo2/fxml/JuegoController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package espol.poo.proyectopoogrupo2.fxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class JuegoController implements Initializable {
+
+    @FXML
+    private BorderPane escena;
+    @FXML
+    private Label enunciado;
+    @FXML
+    private Label tiempo;
+    @FXML
+    private ImageView imgTiempo;
+    @FXML
+    private Button cincuenta;
+    @FXML
+    private Button pComp;
+    @FXML
+    private Button pSalon;
+    @FXML
+    private Label participante;
+    @FXML
+    private Label compañero;
+    @FXML
+    private Button opcionA;
+    @FXML
+    private Label enunciadoA;
+    @FXML
+    private Button opcionB;
+    @FXML
+    private Label EnunciadoB;
+    @FXML
+    private Button OpcionC;
+    @FXML
+    private Label EnunciadoC;
+    @FXML
+    private Button OpcionD;
+    @FXML
+    private Label EnunciadoD;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
