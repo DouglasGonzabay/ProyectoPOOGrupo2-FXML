1afc0fb15df7dbf068cb8884ad19840a760eab28
User <User@DESKTOP-TEV60K7>
2023-08-26 18:41:22 -0500

    Getters and Setters añadidos

--- src/main/java/juego/Juego.java
+++ src/main/java/juego/Juego.java
@@ -13,6 +13,8 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Juego{
@@ -26,17 +28,35 @@ public class Juego{
   private int pCont = 0;
   private int nCom = 0;
   private int nivelMax = 0;
-  private ArrayList<String> ComodinesUsados;
+  private ArrayList<String> comodinesUsados;
   private String premio;
+  private String fecha;
+  private long tiempo;
+  private ArrayList<String> preguntasContestadas;
   
 // que quieres hacer aquí para ayudarte : mira crea el constructor con esos atributos y crea uno solo con un atributo arrayListString mejor ok ( 
-  public Juego(String m, int num, Estudiante participante, Estudiante companero, ArrayList<Pregunta> preguntas){
+  public Juego(Estudiante e, Estudiante c, ArrayList<Pregunta> p){
+      this.fecha = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm:ss a").format(LocalDateTime.now());
+      this.participante = e;
+      this.companero = c;
+      this.tiempo = System.currentTimeMillis();
+      this.nivelMax = 0;
+      this.puntaje = 0;
+      this.comodinesUsados = new ArrayList<>();
+      this.pCont = 0;
+      this.nCom = 0;
+      this.premio = "";
+      this.preguntasContestadas = new ArrayList<>();
+      this.preguntas = new ArrayList<>();
+  }
+  
+  /*public Juego(String m, int num, Estudiante participante, Estudiante companero, ArrayList<Pregunta> preguntas){
     this.materia=m;
     this.numParalelo=num;
     this.participante=participante;
     this.companero=companero;
     this.preguntas=preguntas; 
-  }
+  }*/
   public void escribirReporte(String ruta) throws IOException{
     try(BufferedWriter bff = new BufferedWriter(new FileWriter(ruta,true))){
       String linea;
@@ -54,9 +74,11 @@ public class Juego{
   public Estudiante getCompanero(){
     return companero;
   }
+  /*
   public String getNombreP(){
     return companero.getNombre();
   }
+  */
   public ArrayList<Pregunta> getPreguntas(){
     return preguntas;
   }
@@ -64,25 +86,60 @@ public class Juego{
   public int getPuntaje(){
     return puntaje;
   }
+  public String getFecha(){
+      return fecha;
+  }
+  public int getPcon(){
+      return pCont;
+  }
+  public int getNcont(){
+      return nCom;
+  }
+  public int getNivelMax(){
+      return nivelMax;
+  }
+  public long getTiempo(){
+      return tiempo;
+  }
+  public ArrayList<String> getPreguntasContestadas(){
+      return preguntasContestadas;
+  }
+  public ArrayList<String> getComodinesUsados(){
+      return comodinesUsados;
+  }
   public void setPuntaje(int puntaje){
     this.puntaje += puntaje;
   }
   public void setPcon(){
     pCont+=1;
   }
-  public void setNivelMax(int nivel){
-    nivelMax = nivel;
+  public void setNivelMax(){
+    nivelMax  +=1;
   }
   public void setnCom(){
     nCom +=1;
   }
+  public void setTiempo(){
+      tiempo = (tiempo - System.currentTimeMillis())/1000;
+  }
+  public void agregarComodin(String com){
+      comodinesUsados.add(com);
+  }
+  public void agregarPregunta(String enun){
+      preguntasContestadas.add(enun);
+  }
+  public void setPremio(String p){
+      premio = p;
+  }
+  
   //Para detalle de reporte
   public String generarReporte(){
     //Scanner sc=Scanner(System.in);
     //System.out.println("Ingrese el premio: ");
     //String premio=sc.NextLine();
-    return "Participante: "+ this.getNombreP()+ "/Compañero: "+ companero+"/Nivel Maximo alcanzado: "+nivelMax+"/Preguntas contestadas: "+pCont+ "/Comodines usados: "+nCom+"/PREMIO: "+premio;
-    //sc.close();   
+    //return "Participante: "+ this.getNombreP()+ "/Compañero: "+ companero+"/Nivel Maximo alcanzado: "+nivelMax+"/Preguntas contestadas: "+pCont+ "/Comodines usados: "+nCom+"/PREMIO: "+premio;
+    //sc.close();
+    return "";
   }
 /*
   public ArrayList<String> generarDetallePregunta(Pregunta p){
