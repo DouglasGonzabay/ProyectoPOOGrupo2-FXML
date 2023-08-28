3eecdf083522ab3b444b0c1a6e1e9eeb77c9522e
User <User@DESKTOP-TEV60K7>
2023-08-27 22:19:57 -0500

    Modificación de clase ReportController

--- src/main/java/espol/poo/proyectopoogrupo2/fxml/ReportController.java
+++ src/main/java/espol/poo/proyectopoogrupo2/fxml/ReportController.java
@@ -6,13 +6,19 @@ package espol.poo.proyectopoogrupo2.fxml;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import juego.Juego;
+import modelo.Estudiante;
 /**
  * FXML Controller class
  *
@@ -24,33 +30,64 @@ public class ReportController implements Initializable {
     @FXML
     private Button btsalir2;
     @FXML
-    private TableView<?> tablareport;
+    private TableView<Juego> tablareport;
     @FXML
-    private TableColumn<?, ?> fechacol;
+    private TableColumn<Juego, String> fechacol;
     @FXML
-    private TableColumn<?, ?> participantcol;
+    private TableColumn<Juego, Estudiante> participantcol;
     @FXML
-    private TableColumn<?, ?> levelcol;
+    private TableColumn<Juego, Integer> levelcol;
     @FXML
-    private TableColumn<?, ?> timecol;
+    private TableColumn<Juego, Long> timecol;
     @FXML
-    private TableColumn<?, ?> pregcol;
+    private TableColumn<Juego, Integer> pregcol;
     @FXML
-    private TableColumn<?, ?> comdcol;
+    private TableColumn<Juego,Integer > comdcol;
     @FXML
-    private TableColumn<?, ?> premiocol;
+    private TableColumn<Juego, String> premiocol;
     @FXML
-    private TableColumn<?, ?> detallescol;
+    private TableColumn<Juego, String> detallescol;
+    
+    static ObservableList<Juego> reporte;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        ArrayList<Juego>juego=Juego.leerReportes();
+        ArrayList<Juego>j1=new ArrayList();
+        for(Juego j:juego){
+            String fecha=j.getFecha();
+            Estudiante participante1=j.getParticipante();
+            int pCon=j.getPcon();
+            int numLevel=j.getNivelMax();
+            long timeGame=j.getTiempo();
+            int numComod=j.getPcon();
+            String premio=j.getPremio();
+            String detail="";
+            Juego jg=new Juego(fecha,participante1,numLevel,timeGame,pCon,numComod,premio,detail);
+            j1.add(jg);
+        }
+        reporte=FXCollections.observableArrayList();
+        /*
+        fechacol.setCellValueFactory(new PropertyValueFactory<>("Fecha"));
+        participantcol.setCellValueFactory(new PropertyValueFactory<>("Participante"));
+        levelcol.setCellValueFactory(new PropertyValueFactory<>("Nivel Máximo"));
+        timecol.setCellValueFactory(new PropertyValueFactory<>("Tiempo"));
+        pregcol.setCellValueFactory(new PropertyValueFactory<>("Preguntas Contestadas"));
+        comdcol.setCellValueFactory(new PropertyValueFactory<>("Comodines usados"));
+        premiocol.setCellValueFactory(new PropertyValueFactory<>("Premio"));
+        detallescol.setCellValueFactory(new PropertyValueFactory<>("Detalles"));
+        */
+        reporte.addAll(j1);
+        tablareport.setItems(reporte);
     }
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
-    }    
+    }
+      
+    
     
 }
