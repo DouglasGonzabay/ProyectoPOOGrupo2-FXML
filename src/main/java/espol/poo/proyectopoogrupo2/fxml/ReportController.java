c7ce422002c4b7a436dc47d1d3c3bca70aa5f361
User <User@DESKTOP-TEV60K7>
2023-08-27 23:33:34 -0500

    Modificacion

--- src/main/java/espol/poo/proyectopoogrupo2/fxml/ReportController.java
+++ src/main/java/espol/poo/proyectopoogrupo2/fxml/ReportController.java
@@ -12,11 +12,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import juego.Juego;
 import modelo.Estudiante;
 /**
@@ -29,59 +37,48 @@ public class ReportController implements Initializable {
 
     @FXML
     private Button btsalir2;
-    @FXML
-    private TableView<Juego> tablareport;
-    @FXML
-    private TableColumn<Juego, String> fechacol;
-    @FXML
-    private TableColumn<Juego, Estudiante> participantcol;
-    @FXML
-    private TableColumn<Juego, Integer> levelcol;
-    @FXML
-    private TableColumn<Juego, Long> timecol;
-    @FXML
-    private TableColumn<Juego, Integer> pregcol;
-    @FXML
-    private TableColumn<Juego,Integer > comdcol;
-    @FXML
-    private TableColumn<Juego, String> premiocol;
-    @FXML
-    private TableColumn<Juego, String> detallescol;
     
-    static ObservableList<Juego> reporte;
+    //static ObservableList<Juego> reporte;
+    @FXML
+    private VBox visParticipantes;
+    @FXML
+    private VBox visualizarInfo;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        ArrayList<Juego>juego=Juego.leerReportes();
-        ArrayList<Juego>j1=new ArrayList();
-        for(Juego j:juego){
-            String fecha=j.getFecha();
-            Estudiante participante1=j.getParticipante();
-            int pCon=j.getPcon();
-            int numLevel=j.getNivelMax();
-            long timeGame=j.getTiempo();
-            int numComod=j.getPcon();
-            String premio=j.getPremio();
-            String detail="";
-            Juego jg=new Juego(fecha,participante1,numLevel,timeGame,pCon,numComod,premio,detail);
-            j1.add(jg);
+        ArrayList<Juego> reportes = Juego.leerReportes();
+        for(Juego j: reportes){
+            Label l = new Label(j.getParticipante().toString());
+            l.setOnMouseClicked(eh->{
+            
+                visualizarInfo.getChildren().clear();
+                Label fecha = new Label("Fecha: " + j.getFecha());
+                Label nombre = new Label("Participante: "+j.getParticipante().getNombre());
+                Label companero = new Label("Compañero: " + j.getCompanero().getNombre());
+                Label nivelM = new Label("Nivel Máximo Alcanzado: "+j.getNivelMax());
+                Label tiempo = new Label("Tiempo: " + j.getTiempo());
+                Label contestadas = new Label("Preguntas Contestadas: " + j.getPcon());
+                Label comodines = new Label("Comodines usados: " + j.getNcont());
+                Label premio = new Label("Premio entregado: " + j.getPremio());
+                VBox preguntas = new VBox(5);
+                for(String s: j.getPreguntasContestadas()){
+                    VBox h = new VBox(1);
+                    Label n1 = new Label("- "+s);
+                    n1.setWrapText(true);
+                    Label n2 = new Label("Copmodin usado: "+j.getComodinesUsados().get(j.getPreguntasContestadas().indexOf(s)));
+                    n2.setWrapText(true);
+                    h.getChildren().addAll(n1,n2);
+                    preguntas.getChildren().add(h);
+                }
+                preguntas.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+                //preguntas.setMaxWidth(1500);
+                visualizarInfo.getChildren().addAll(fecha,nombre,companero,nivelM,tiempo,contestadas,comodines,premio,new Label("Pregunta Contestada y su comodin: "),preguntas);
+            });
+            visParticipantes.getChildren().add(l);
         }
-        reporte=FXCollections.observableArrayList();
-        /*
-        fechacol.setCellValueFactory(new PropertyValueFactory<>("Fecha"));
-        participantcol.setCellValueFactory(new PropertyValueFactory<>("Participante"));
-        levelcol.setCellValueFactory(new PropertyValueFactory<>("Nivel Máximo"));
-        timecol.setCellValueFactory(new PropertyValueFactory<>("Tiempo"));
-        pregcol.setCellValueFactory(new PropertyValueFactory<>("Preguntas Contestadas"));
-        comdcol.setCellValueFactory(new PropertyValueFactory<>("Comodines usados"));
-        premiocol.setCellValueFactory(new PropertyValueFactory<>("Premio"));
-        detallescol.setCellValueFactory(new PropertyValueFactory<>("Detalles"));
-        */
-        reporte.addAll(j1);
-        tablareport.setItems(reporte);
     }
     @FXML
     private void switchToPrimary() throws IOException {
